7b7842020683f5d541e78d7c633edd9381914150
Daniel <krzyczkowski.daniel@op.pl>
2018-05-19 23:58:53 +0200

    SignalR Transport App Simulator commited.

---
+++ TransportAppSimulator/TransportAppSimulator/LocationUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportAppSimulator
+{
+    public class LocationUpdate
+    {
+        public string DriverName;
+        public double Latitude = 52.2326;
+        public double Longitude = 20.7810;
+    }
+}
---
+++ TransportAppSimulator/TransportAppSimulator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace TransportAppSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hubClient = new ClientSignalR();
+            hubClient.Initialize("http://localhost:63369/transport");
+            var locationUpdate = new LocationUpdate()
+            {
+                DriverName = "Daniel"
+            };
+
+            var locationUpdate2 = new LocationUpdate()
+            {
+                DriverName = "Adam",
+                Latitude = 52.28107,
+                Longitude = 20.9558243
+            };
+
+            Observable
+            .Interval(TimeSpan.FromSeconds(2))
+            .Subscribe(
+                async x =>
+                {
+                    await hubClient.SendHubMessage("broadcastMessage", locationUpdate);
+                    Console.WriteLine("SENDING LOCATION UPDATE: " + locationUpdate.DriverName + " " + locationUpdate.Latitude + " " + locationUpdate.Longitude);
+                    await hubClient.SendHubMessage("broadcastMessage", locationUpdate2);
+                    Console.WriteLine("SENDING LOCATION UPDATE: " + locationUpdate2.DriverName + " " + locationUpdate2.Latitude + " " + locationUpdate2.Longitude);
+
+
+                    locationUpdate.Latitude = locationUpdate.Latitude + 0.0008;
+                    locationUpdate.Longitude = locationUpdate.Longitude + 0.0008;
+
+                    locationUpdate2.Latitude = locationUpdate2.Latitude + 0.0008;
+                    locationUpdate2.Longitude = locationUpdate2.Longitude + 0.0008;
+                });
+
+            Console.ReadKey();
+        }
+    }
+}
