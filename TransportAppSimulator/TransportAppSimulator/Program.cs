620a0f68100b0e6865b810e059f748c267e5e08e
Daniel <danek22@op.pl>
2018-07-31 07:27:18 +0200

    TransportAppSimulator project updated.

--- TransportAppSimulator/TransportAppSimulator/Program.cs
+++ TransportAppSimulator/TransportAppSimulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reactive.Linq;
 
 namespace TransportAppSimulator
@@ -7,36 +9,75 @@ namespace TransportAppSimulator
     {
         static void Main(string[] args)
         {
+            List<LocationUpdate> locationUpdates = new List<LocationUpdate>
+            {
+             new LocationUpdate
+                {
+                    Latitude = 52.23292,
+                    Longitude = 20.99114,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 52.25229,
+                    Longitude = 20.94341,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 52.19462,
+                    Longitude = 20.82109,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 52.09011,
+                    Longitude = 20.36584,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 52.01813,
+                    Longitude = 19.97684,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 51.87744,
+                    Longitude = 19.64773,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 51.13844,
+                    Longitude = 16.93472,
+                    DriverName = "Daniel"
+                },
+             new LocationUpdate
+                {
+                    Latitude = 51.12705,
+                    Longitude = 16.92196,
+                    DriverName = "Daniel"
+                },
+            };
+
             var hubClient = new ClientSignalR();
             hubClient.Initialize("http://localhost:63369/transport");
-            var locationUpdate = new LocationUpdate()
-            {
-                DriverName = "Daniel"
-            };
-
-            var locationUpdate2 = new LocationUpdate()
-            {
-                DriverName = "Adam",
-                Latitude = 52.28107,
-                Longitude = 20.9558243
-            };
 
             Observable
-            .Interval(TimeSpan.FromSeconds(2))
+            .Interval(TimeSpan.FromSeconds(3))
             .Subscribe(
                 async x =>
                 {
-                    await hubClient.SendHubMessage("broadcastMessage", locationUpdate);
-                    Console.WriteLine("SENDING LOCATION UPDATE: " + locationUpdate.DriverName + " " + locationUpdate.Latitude + " " + locationUpdate.Longitude);
-                    await hubClient.SendHubMessage("broadcastMessage", locationUpdate2);
-                    Console.WriteLine("SENDING LOCATION UPDATE: " + locationUpdate2.DriverName + " " + locationUpdate2.Latitude + " " + locationUpdate2.Longitude);
-
-
-                    locationUpdate.Latitude = locationUpdate.Latitude + 0.0008;
-                    locationUpdate.Longitude = locationUpdate.Longitude + 0.0008;
-
-                    locationUpdate2.Latitude = locationUpdate2.Latitude + 0.0008;
-                    locationUpdate2.Longitude = locationUpdate2.Longitude + 0.0008;
+                    var locationUpdate = locationUpdates.FirstOrDefault();
+                    if(locationUpdate !=null)
+                    {
+                        await hubClient.SendHubMessage("broadcastMessage", locationUpdate);
+                        Console.WriteLine("SENDING LOCATION UPDATE: " + locationUpdate.DriverName + " " + locationUpdate.Latitude + " " + locationUpdate.Longitude);
+                        locationUpdates.Remove(locationUpdate);
+                    }
+                    else
+                    Console.WriteLine("UPDATES COMPLETED");
                 });
 
             Console.ReadKey();
